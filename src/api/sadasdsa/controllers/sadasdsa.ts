da72727b3a646cc625fc17d575b3f79e3bc93664
bnn0s <yabaharev55@mail.ru>
2024-01-27 18:18:05 +0300

    first commit

---
+++ src/api/sadasdsa/controllers/sadasdsa.ts
@@ -0,0 +1,7 @@
+/**
+ * sadasdsa controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sadasdsa.sadasdsa');
---
+++ src/api/sadasdsa/routes/sadasdsa.ts
@@ -0,0 +1,7 @@
+/**
+ * sadasdsa router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sadasdsa.sadasdsa');
